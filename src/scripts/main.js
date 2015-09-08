82c0786822ca320216a3aceabb8dac1b6a214fee
Paul Heasley <paul.heasley@gmail.com>
2015-09-09 09:51:41 +1000

    Initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+// Bespoke.js
+bespoke.from('article', [
+    bespoke.plugins.classes(),
+    //bespoke.plugins.bullets('li, .bullet'),
+    //bespoke.plugins.backdrop(),
+    bespoke.plugins.scale(),
+    bespoke.plugins.hash(),
+    bespoke.plugins.progress(),
+    //bespoke.plugins.state(),
+    bespoke.plugins.keys(),
+    //bespoke.plugins.touch()
+]);
