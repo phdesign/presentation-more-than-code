a48b96c0839f07ccbd18a19c2447cec0f151e126
Paul Heasley <paul.heasley@gmail.com>
2015-09-09 22:02:55 +1000

    Preload images

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,24 @@ bespoke.from('article', [
     bespoke.plugins.keys(),
     //bespoke.plugins.touch()
 ]);
+
+(function preloadBackgroundImages() {
+
+    var matches, image,
+    forEach = function(arrayLike, fn) {
+        [].slice.call(arrayLike, 0).forEach(fn);
+    };
+
+    forEach(document.styleSheets, function(sheet) {
+        forEach(sheet.rules, function(rule) {
+            if (rule.style && rule.style.backgroundImage) {
+                matches = rule.style.backgroundImage.match(/url\((.*)\)/);
+                if (matches) {
+                    image = new Image();
+                    image.src = matches[1];
+                }
+            }
+        });
+    });
+
+}());
