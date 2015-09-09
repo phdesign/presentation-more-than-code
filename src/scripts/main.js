f2b019109ca061880a3a412b4285ee9cc4032f23
Paul Heasley <paul.heasley@gmail.com>
2015-09-09 16:47:32 +1000

    Added skill tree background

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 // Bespoke.js
 bespoke.from('article', [
     bespoke.plugins.classes(),
-    //bespoke.plugins.bullets('li, .bullet'),
-    //bespoke.plugins.backdrop(),
+    bespoke.plugins.bullets('li, .bullet'),
+    bespoke.plugins.backdrop(),
     bespoke.plugins.scale(),
     bespoke.plugins.hash(),
     bespoke.plugins.progress(),
-    //bespoke.plugins.state(),
+    bespoke.plugins.state(),
     bespoke.plugins.keys(),
     //bespoke.plugins.touch()
 ]);
